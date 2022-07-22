65011420dbc7914d0d85f7ed5b968b2fef4938b7
Mirwais Farahi <waisfarahi@gmail.com>
2022-07-22 17:32:30 +0500

    Refactor  Calculator.js File with react hooks

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,82 +1,78 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { Container, Row, Col } from 'reactstrap';
 import calculate from '../logic/calculate';
 
-class Calculator extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      total: '',
-      next: null,
-      operation: null,
+function Calculator() {
+  const [state, setState] = useState({
+    total: '',
+    next: null,
+    operation: null,
+  });
 
-    };
-  }
-
-  operationHandler = (x) => {
-    this.setState((s) => calculate(s, x));
+  const operationHandler = (e) => {
+    const pressedButton = e.target.innerText;
+    const newState = calculate(state, pressedButton);
+    setState(newState);
   };
 
-  render() {
-    const { total, operation, next } = this.state;
+  const { next, total, operation } = state;
 
-    return (
-      <Container className="calculator">
-        <Row className="display-box">
-          <Col className="bg-secondary border result">
-            <span>
-              {' '}
-              {total}
-              {' '}
-            </span>
-            <span>
-              {' '}
-              {operation}
-              {' '}
-            </span>
-            <span>
-              {' '}
-              {next}
-              {' '}
-            </span>
-          </Col>
-        </Row>
-        <Row>
-          <Col className="light column border" onClick={() => this.operationHandler('AC')}>AC</Col>
-          <Col className="light column border" onClick={() => this.operationHandler('+/-')}>+/-</Col>
-          <Col className="light column border" onClick={() => this.operationHandler('%')}>%</Col>
-          <Col className="orange column border" onClick={() => this.operationHandler('÷')}>÷</Col>
-        </Row>
-        <Row>
-          <Col className="light column border" onClick={() => this.operationHandler('7')}>7</Col>
-          <Col className="light column border" onClick={() => this.operationHandler('8')}>8</Col>
-          <Col className="light column border" onClick={() => this.operationHandler('9')}>9</Col>
-          <Col className="orange column border" onClick={() => this.operationHandler('x')}>x</Col>
-        </Row>
-        <Row>
-          <Col className="light column border" onClick={() => this.operationHandler('4')}>4</Col>
-          <Col className="light column border" onClick={() => this.operationHandler('5')}>5</Col>
-          <Col className="light column border" onClick={() => this.operationHandler('6')}>6</Col>
-          <Col className="orange column border" onClick={() => this.operationHandler('-')}>-</Col>
-        </Row>
-        <Row>
-          <Col className="light column border" onClick={() => this.operationHandler('1')}>1</Col>
-          <Col className="light column border" onClick={() => this.operationHandler('2')}>2</Col>
-          <Col className="light column border" onClick={() => this.operationHandler('3')}>3</Col>
-          <Col className="orange column border" onClick={() => this.operationHandler('+')}>+</Col>
-        </Row>
-        <Row>
-          <Col className="light column border" onClick={() => this.operationHandler('0')}>0</Col>
-          <Col className="light">
-            <Row>
-              <Col className="light column border" onClick={() => this.operationHandler('.')}>.</Col>
-              <Col className="orange column border" onClick={() => this.operationHandler('=')}>=</Col>
-            </Row>
-          </Col>
-        </Row>
-      </Container>
-    );
-  }
+  return (
+    <Container className="calculator">
+      <Row className="display-box">
+        <Col className="bg-secondary border result">
+          <span>
+            {' '}
+            {total}
+            {' '}
+          </span>
+          <span>
+            {' '}
+            {operation}
+            {' '}
+          </span>
+          <span>
+            {' '}
+            {next}
+            {' '}
+          </span>
+        </Col>
+      </Row>
+      <Row>
+        <Col className="light column border" onClick={operationHandler}>AC</Col>
+        <Col className="light column border" onClick={operationHandler}>+/-</Col>
+        <Col className="light column border" onClick={operationHandler}>%</Col>
+        <Col className="orange column border" onClick={operationHandler}>÷</Col>
+      </Row>
+      <Row>
+        <Col className="light column border" onClick={operationHandler}>7</Col>
+        <Col className="light column border" onClick={operationHandler}>8</Col>
+        <Col className="light column border" onClick={operationHandler}>9</Col>
+        <Col className="orange column border" onClick={operationHandler}>x</Col>
+      </Row>
+      <Row>
+        <Col className="light column border" onClick={operationHandler}>4</Col>
+        <Col className="light column border" onClick={operationHandler}>5</Col>
+        <Col className="light column border" onClick={operationHandler}>6</Col>
+        <Col className="orange column border" onClick={operationHandler}>-</Col>
+      </Row>
+      <Row>
+        <Col className="light column border" onClick={operationHandler}>1</Col>
+        <Col className="light column border" onClick={operationHandler}>2</Col>
+        <Col className="light column border" onClick={operationHandler}>3</Col>
+        <Col className="orange column border" onClick={operationHandler}>+</Col>
+      </Row>
+      <Row>
+        <Col className="light column border" onClick={operationHandler}>0</Col>
+        <Col className="light">
+          <Row>
+            <Col className="light column border" onClick={operationHandler}>.</Col>
+            <Col className="orange column border" onClick={operationHandler}>=</Col>
+          </Row>
+        </Col>
+      </Row>
+    </Container>
+  );
 }
 
 export default Calculator;
